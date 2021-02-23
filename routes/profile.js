ef8b537c711322ec651d4859584895be370f52e6
LUIS ESPINOZA <coco@LUISs-MacBook-Pro.local>
2021-02-23 10:44:29 -0800

    commented out Howard's db call until db is established

--- routes/profile.js
+++ routes/profile.js
@@ -24,17 +24,17 @@ router.get("/:user", (req,res)=> {
 
 //Post "/meeting/new"
 router.post("/", (req,res) => {
-  db.meeting.create({
-    user: req.body.user,
-    url: req.body.url,
-    dateTime: req.body.dateTime,
-    // private is a boolean value
-    private: req.body.private,
-    // daily, weekly, or monthly
-    recurring: req.body.recurring,
-    passcode: req.body.passcode,
-    notes: req.body.notes,
-  })
+  // db.meeting.create({
+  //   user: req.body.user,
+  //   url: req.body.url,
+  //   dateTime: req.body.dateTime,
+  //   // private is a boolean value
+  //   private: req.body.private,
+  //   // daily, weekly, or monthly
+  //   recurring: req.body.recurring,
+  //   passcode: req.body.passcode,
+  //   notes: req.body.notes,
+  // })
 })
 //Get "/meeting/:id"
 router.get()
