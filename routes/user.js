72e9a9caa45ae51e9c948504bdee7939d7e16a23
howardnguyen714 <howardnguyen714@gmail.com>
2021-02-23 10:04:49 -0800

    started working on create meeting route

--- routes/user.js
+++ routes/user.js
@@ -8,7 +8,19 @@ router.use(methodOverride('_method'));
 //Get "/"
 router.get()
 //Post "/meeting/new"
-router.post()
+router.post("/", (req,res) => {
+  db.meeting.create({
+    user: req.body.user,
+    url: req.body.url,
+    dateTime: req.body.dateTime,
+    // private is a boolean value
+    private: req.body.private,
+    // daily, weekly, or monthly
+    recurring: req.body.recurring,
+    passcode: req.body.passcode,
+    notes: req.body.notes,
+  })
+})
 //Get "/meeting/:id"
 router.get()
 //Put "/meeting/:id/edit"
