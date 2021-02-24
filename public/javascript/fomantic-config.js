6113238b0ad0625b1908f736452f29aa171e2416
LUIS ESPINOZA <coco@LUISs-MacBook-Pro.local>
2021-02-24 15:18:22 -0800

    class reassignment for modal

--- public/javascript/fomantic-config.js
+++ public/javascript/fomantic-config.js
@@ -1,6 +1,16 @@
 $( document ).ready(function() {
   $('table').tablesort();
-  $('#newMeeting').click(function(){
+
+  // let meetingId = "newMeeting"
+  // let meetingId = null
+  // $(document).ready(function () {
+  //   $('.modal-selection-helper').mouseenter(function () {
+  //     // "this" now referrs to button you hovered
+  //     meetingId = $(this).attr("id")
+  //     // alert(meetingId);
+  //   });
+  // });
+  $( `.modal-toggle`).click(function(){
     $('.ui.modal').modal('show');
     $('#standard_calendar').calendar();
     $('.ui.checkbox').checkbox();
