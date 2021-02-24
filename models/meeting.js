8dbf4f3055e372495530f095a14c51222bf9b716
Aaron Goldsmith <aarongoldsmith1@outlook.com>
2021-02-23 20:30:48 -0800

    add hasOne association to meeting model for user

--- models/meeting.js
+++ models/meeting.js
@@ -10,6 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
+      models.meeting.hasOne(models.user)
       models.meeting.belongsToMany(models.category, 
         { through: 'meetingsCategories' });
     }
