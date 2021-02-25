f7379c4866e3a282dff374d15a08995fdbb268c2
Aaron Goldsmith <aarongoldsmith1@outlook.com>
2021-02-25 12:01:24 -0800

    add meeting and profile controllers

--- server.js
+++ server.js
@@ -44,18 +44,14 @@ app.use((req, res, next) => {
   next()
 })
 
-
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/profile', isLoggedIn, (req, res) => {
-  res.render('profile');
-});
-
 app.use('/auth', require('./routes/auth'));
-app.use("/meetings",require("./routes/meetings"))
-app.use("/user",require("./routes/profile"))
+app.use('/meetings', require('./routes/meetings'))
+app.use('/profile', require('./routes/profile'))
+
 var server = app.listen(process.env.PORT || 3000, ()=> console.log(`🎧You're listening to the smooth sounds of port ${process.env.PORT || 3000}🎧`));
 
 module.exports = server;
