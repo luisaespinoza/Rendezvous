e7c799e19a5254c8c4f799a659c4ab03f7a74467
LUIS ESPINOZA <coco@LUISs-MacBook-Pro.local>
2021-02-25 13:42:27 -0800

    delete functionality reimplemented. Todo: Repair calendar

--- public/javascript/fomantic-config.js
+++ public/javascript/fomantic-config.js
@@ -1,24 +1,27 @@
 $( document ).ready(function() {
-  $('.mini.modal').modal('show');
   $('table').tablesort();
-
+  
   // $( `.modal-toggle`).click(function(event){
-  //   let id = event.target.attributes.value.value
-  //   console.log(event)
-  //   console.log(id)
-  //   $(id).modal('show');
-  //   $('#standard_calendar').calendar();
-  //   $('.ui.checkbox').checkbox();
-  //   $('.selection.dropdown').dropdown();
-  // })
-
-  $( `.modal-toggle`).click(function(event){
-    let id = "#meeting" + event.target.attributes.value.value
-    let caldendarId = "#calendar" + event.target.attributes.value.value
-    $(id).modal('show');
-    $(caldendarId).calendar();
-    $('.ui.checkbox').checkbox();
-    $('.selection.dropdown').dropdown();
-  })
-
+    //   let id = event.target.attributes.value.value
+    //   console.log(event)
+    //   console.log(id)
+    //   $(id).modal('show');
+    //   $('#standard_calendar').calendar();
+    //   $('.ui.checkbox').checkbox();
+    //   $('.selection.dropdown').dropdown();
+    // })
+    
+    $( `.modal-toggle`).click(function(event){
+      let id = "#meeting" + event.target.attributes.value.value
+      let caldendarId = "#calendar" + event.target.attributes.value.value
+      $(id).modal('show');
+      $(caldendarId).calendar();
+      $('.ui.checkbox').checkbox();
+      $('.selection.dropdown').dropdown();
+    })
+    $(".delete-toggle").click(function(event){
+      let id = "#delete" + event.target.attributes.value.value
+      $(id).modal('show');
+    })
+    
 });
