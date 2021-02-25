97a687ed99541c19dc69647bc2d934da4f40f287
LUIS ESPINOZA <coco@LUISs-MacBook-Pro.local>
2021-02-25 12:22:28 -0800

    changed before howard's pull

--- public/javascript/fomantic-config.js
+++ public/javascript/fomantic-config.js
@@ -1,18 +1,23 @@
 $( document ).ready(function() {
   $('table').tablesort();
-  // $( `.modal-toggle`).click(function(){
-  //   $('.ui.modal').modal('show');
+
+  // $( `.modal-toggle`).click(function(event){
+  //   let id = event.target.attributes.value.value
+  //   console.log(event)
+  //   console.log(id)
+  //   $(id).modal('show');
   //   $('#standard_calendar').calendar();
   //   $('.ui.checkbox').checkbox();
   //   $('.selection.dropdown').dropdown();
   // })
+
   $( `.modal-toggle`).click(function(event){
-    let id = event.target.attributes.value.value
-    console.log(event)
-    console.log(id)
+    let id = "#meeting" + event.target.attributes.value.value
+    let caldendarId = "#calendar" + event.target.attributes.value.value
     $(id).modal('show');
-    $('#standard_calendar').calendar();
+    $(caldendarId).calendar();
     $('.ui.checkbox').checkbox();
     $('.selection.dropdown').dropdown();
   })
+
 });
