78733a1aec97bf936d5c95f9a1d506d7c17b5c51
Aaron Goldsmith <aarongoldsmith1@outlook.com>
2021-02-26 11:50:25 -0800

    update calendar, prevent date selection

--- public/javascript/fomantic-config.js
+++ public/javascript/fomantic-config.js
@@ -1,32 +1,7 @@
 $(document).ready(function () {
   $('table').tablesort();
 
-  // $( `.modal-toggle`).click(function(event){
-  //   let id = event.target.attributes.value.value
-  //   console.log(event)
-  //   console.log(id)
-  //   $(id).modal('show');
-  //   $('#standard_calendar').calendar();
-  //   $('.ui.checkbox').checkbox();
-  //   $('.selection.dropdown').dropdown();
-  // })
-
-  // $( `.modal-toggle`).click(function(event){
-  //   let id = "#meeting" + event.target.attributes.value.value
-  //   let calendarId = "#calendar" + event.target.attributes.value.value
-  //   console.log(calendarId)
-  //   $(id).modal('show');
-  //   $(calendarId).calendar();
-  //   $('.ui.checkbox').checkbox();
-  //   $('.selection.dropdown').dropdown();
-  // })
-  // $(".delete-toggle").click(function(event){
-  //   let id = "#delete" + event.target.attributes.value.value
-  //   $(id).modal('show');
-  // })
-
-
-  $(`.modal-toggle`).click(function (event) {
+  $('.modal-toggle').click(function (event) {
     let id = "#meeting" + event.target.attributes.value.value
     let calendarId = "#calendar" + event.target.attributes.value.value
     let preSelectedDate = $(calendarId).attr("value")
@@ -64,17 +39,13 @@ $(document).ready(function () {
   $('#userMeetings').children('div').each(function () {
     let time = this.dataset.date.substring(16,34)
     meetings.push({date: new Date(this.dataset.date), message: this.dataset.title + " |\n" + time})
-    // console.log(this.dataset.title,this.dataset.date); // "this" is the current element in the loop
   });
-  console.log(meetings)
 
-  $('#inline_calendar')
-  .calendar(
-    {
+  $('#inline_calendar').calendar({
       eventClass: "green",
-      eventDates: meetings 
-    }
-  )
-;
-
+      eventDates: meetings,
+      onSelect: function() {
+        return false;
+      }
+    });
 });
