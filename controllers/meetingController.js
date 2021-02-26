04392999cbd7ae935041e5da253ea8413be64515
LUIS ESPINOZA <coco@LUISs-MacBook-Pro.local>
2021-02-26 11:57:56 -0800

    housekeeping: cleaned up file structure/routes/controllers to reflect changes in our app

--- controllers/meetingController.js
+++ controllers/meetingController.js
@@ -105,10 +105,23 @@ async function deleteMeeting (req, res) {
   }
 }
 
+async function getCalendar(req, res) {
+  const userMeetings = await db.meeting.findAll({ where: { userId: req.user.dataValues.id } })
+
+  userMeetings.forEach((meeting) => {
+    meeting.dataValues.dateTime = new Date(meeting.dataValues.dateTime)
+    meeting.dataValues.dateTime = new moment(meeting.dataValues.dateTime).format('MMMM/D/YYYY h:mm a')
+  })
+  // console.log(userMeetings)
+  // const meetings = await db.meetings.findAll({where: {id: }})
+  res.render('meetings/calendar', {userMeetings});
+}
+
 module.exports = {
   createMeeting,
   deleteMeeting,
   getMeetings,
   getMeetingInfo,
   updateMeeting,
+  getCalendar
 }
--- routes/profile.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const isLoggedIn = require('../middleware/isLoggedIn');
-
-const profileController = require('../controllers/profileController')
-
-router.get('/', isLoggedIn, profileController.getProfile);
-
-module.exports = router;
