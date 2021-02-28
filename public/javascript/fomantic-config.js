a6752804db96d2f911bf293e5e0cf1e3f4aaff1a
LUIS ESPINOZA <coco@LUISs-MacBook-Pro.local>
2021-02-27 16:40:42 -0800

    added multiple events to single calendar day. TODO: sort those events by time

--- public/javascript/fomantic-config.js
+++ public/javascript/fomantic-config.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
           date: function (date, settings) {
             if (!date) return '';
             var day = date.getDate();
-            var month = date.getMonth()+1;
+            var month = date.getMonth() + 1;
             var year = date.getFullYear();
             return month + '/' + day + '/' + year;
           }
@@ -37,15 +37,56 @@ $(document).ready(function () {
   })
 
   let meetings = []
+  let meets = []
+  let meetsTemp = []
   $('#userMeetings').children('div').each(function () {
+    let isUnique = true
+    let indexOfMatch = null
+    let date = new Date(this.dataset.date)
+    let tempDate2 = date
     let time = this.dataset.date.substring(this.dataset.date.length - 8)
-    meetings.push({date: new Date(this.dataset.date), message: this.dataset.title + " |\n" + time})
+    
+    tempDate2.setHours(0, 0, 0, 0)
+
+    console.log(date)
+    console.log("---------------", tempDate2)
+    
+    meets.forEach(dateObj => {
+      let tempDate = dateObj
+      tempDate.setHours(0, 0, 0, 0)
+      console.log("++++++++++++", tempDate)
+
+      if (tempDate.valueOf() === tempDate2.valueOf()) {
+        indexOfMatch = meets.indexOf(dateObj)
+        isUnique = false
+      }
+
+    });
+
+
+
+    if (isUnique) {
+      meets.push(date)
+      meetings.push({ date: new Date(date), message: this.dataset.title + " : " + time })
+    } else {
+      meetings[indexOfMatch].message += " | " + this.dataset.title + " : " + time
+    }
+
+
+
+    // let time = this.dataset.date.substring(this.dataset.date.length - 8)
+    // meetings.push({ date: new Date(this.dataset.date), message: this.dataset.title + " |\n" + time })
   });
+
+  meetings.sort(function(date1,date2){
+    return date1.date.getTime() - date2.date.getTime()
+  })
+
 
   $('#inline_calendar').calendar({
     eventClass: "green",
     eventDates: meetings,
-    onSelect: function(date, mode) {
+    onSelect: function (date, mode) {
       if (mode === 'day') return false;
     }
   });
